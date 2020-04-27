7f2bb121119d399076cf051669b86a1980e340d6
Pravin Bansal <pravin.bansal@getzephy.com>
2020-04-26 23:29:26 -0700

    making mongo container name specific to avoid clash with existing containers.

--- case-service/util/connection.js
+++ case-service/util/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = "mongodb://mongo:27017/ehr-case";
+const connection = "mongodb://mongo-ehr:27017/ehr-case";
 
 const connectDb = () => {
   return mongoose.connect(connection);
--- user-service/util/connection.js
+++ user-service/util/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const User = require("../model/User.model");
 
-const connection = "mongodb://mongo:27017/ehr-user";
+const connection = "mongodb://mongo-ehr:27017/ehr-user";
 
 const connectDb = () => {
   return mongoose.connect(connection);
